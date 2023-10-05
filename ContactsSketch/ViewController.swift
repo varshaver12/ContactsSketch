82e2b9fce1428315541c3cb383d20092839892b4
khlestkinaleksey <khlestkinvarshaveraleksey@gmail.com>
2023-10-05 18:23:48 +0300

    Initial Commit

---
+++ ContactsSketch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsSketch
+//
+//  Created by 1234 on 05.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
